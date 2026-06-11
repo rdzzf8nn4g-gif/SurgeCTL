2eb56767851e0d795646ed941494c1b15a6636d2
rdzzf8nn4g-gif <rdzzf8nn4g@privaterelay.appleid.com>
2026-06-11 18:13:58 +0800

    Update SurgeCCDirect.m

--- SurgeCCDirect.m
+++ SurgeCCDirect.m
@@ -6,8 +6,27 @@
 
 @implementation SurgeCCDirect
 
+// 核心辅助方法：把任意尺寸的 SF Symbol 画死在一个 50x50 的绝对正方形画布正中心
+- (UIImage *)centeredImageWithSymbolName:(NSString *)name {
+    UIImageSymbolConfiguration *config = [UIImageSymbolConfiguration configurationWithPointSize:26 weight:UIImageSymbolWeightMedium];
+    UIImage *sysImage = [UIImage systemImageNamed:name withConfiguration:config];
+    if (!sysImage) return nil;
+    
+    CGSize canvasSize = CGSizeMake(50.0, 50.0);
+    UIGraphicsImageRenderer *renderer = [[UIGraphicsImageRenderer alloc] initWithSize:canvasSize];
+    UIImage *centeredImage = [renderer imageWithActions:^(UIGraphicsImageRendererContext * _Nonnull context) {
+        CGSize imgSize = sysImage.size;
+        CGRect rect = CGRectMake((canvasSize.width - imgSize.width) / 2.0,
+                                 (canvasSize.height - imgSize.height) / 2.0,
+                                 imgSize.width,
+                                 imgSize.height);
+        [sysImage drawInRect:rect];
+    }];
+    return [centeredImage imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+}
+
 - (UIImage *)iconGlyph {
-    return [UIImage systemImageNamed:@"location.fill"];
+    return [self centeredImageWithSymbolName:@"location.fill"]; // 直连图标
 }
 
 - (UIColor *)selectedColor {
@@ -15,12 +34,13 @@
 }
 
 - (BOOL)isSelected {
-    return NO;
+    return NO; // 触发器模式，不保持常亮状态
 }
 
 - (void)setSelected:(BOOL)selected {
     [super setSelected:selected];
     
+    // 异步发送请求，防止卡死 SpringBoard
     NSURL *url = [NSURL URLWithString:@"http://127.0.0.1:1836/v1/outbound"];
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
     request.HTTPMethod = @"POST";
@@ -31,7 +51,7 @@
     
     NSURLSessionTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
         dispatch_async(dispatch_get_main_queue(), ^{
-            [super setSelected:NO]; // 请求发送后恢复按钮状态
+            [super setSelected:NO]; // 请求发送完毕后恢复按钮默认状态
         });
     }];
     [task resume];
