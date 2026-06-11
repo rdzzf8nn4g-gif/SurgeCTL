c426163c6426646c65f8941d15601128f5f95e42
rdzzf8nn4g-gif <rdzzf8nn4g@privaterelay.appleid.com>
2026-06-11 21:14:52 +0800

    Update SurgeCCProxy.m

--- SurgeCCProxy.m
+++ SurgeCCProxy.m
@@ -1,5 +1,6 @@
 #import <UIKit/UIKit.h>
 #import <ControlCenterUIKit/CCUIToggleModule.h>
+#import <CoreFoundation/CoreFoundation.h>
 
 #if __has_include(<roothide.h>)
 #import <roothide.h>
@@ -8,33 +9,30 @@
 #endif
 
 @interface SurgeCCProxy : CCUIToggleModule {
-    BOOL _isActuallySelected;
+    @public BOOL _isActuallySelected;
     NSTimeInterval _lastFetchTime;
 }
 @end
+
+static void ProxyTurnOffCallback(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo) {
+    SurgeCCProxy *module = (__bridge SurgeCCProxy *)observer;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (module->_isActuallySelected) {
+            module->_isActuallySelected = NO;
+            [module refreshState];
+        }
+    });
+}
 
 @implementation SurgeCCProxy
 
 - (instancetype)init {
     self = [super init];
     if (self) {
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(syncStateFromNotification:) name:@"SurgeModeSync" object:nil];
+        CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), (__bridge const void *)(self), ProxyTurnOffCallback, CFSTR("com.crctdd.surgectl.selectDirect"), NULL, CFNotificationSuspensionBehaviorDeliverImmediately);
+        CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), (__bridge const void *)(self), ProxyTurnOffCallback, CFSTR("com.crctdd.surgectl.selectRule"), NULL, CFNotificationSuspensionBehaviorDeliverImmediately);
     }
     return self;
-}
-
-- (void)syncStateFromNotification:(NSNotification *)notification {
-    NSString *activatedMode = notification.object;
-    if (activatedMode && ![activatedMode isEqualToString:@"proxy"]) {
-        if (_isActuallySelected) {
-            _isActuallySelected = NO;
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [self refreshState];
-            });
-        }
-    }
-    _lastFetchTime = 0;
-    [self fetchCurrentStateAsynchronously];
 }
 
 - (NSString *)getRealPrefsPath {
@@ -42,18 +40,14 @@
 #if __has_include(<roothide.h>)
     return jbroot(basePath);
 #else
-    if ([[NSFileManager defaultManager] fileExistsAtPath:@"/var/jb/"]) {
-        return [@"/var/jb" stringByAppendingPathComponent:basePath];
-    }
+    if ([[NSFileManager defaultManager] fileExistsAtPath:@"/var/jb/"]) { return [@"/var/jb" stringByAppendingPathComponent:basePath]; }
     return basePath;
 #endif
 }
 
 - (NSString *)getSetting:(NSString *)key fallback:(NSString *)fallback {
     NSDictionary *prefs = [NSDictionary dictionaryWithContentsOfFile:[self getRealPrefsPath]];
-    if (prefs && prefs[key] && ![prefs[key] isEqual:@""]) {
-        return [NSString stringWithFormat:@"%@", prefs[key]];
-    }
+    if (prefs && prefs[key] && ![prefs[key] isEqual:@""]) { return [NSString stringWithFormat:@"%@", prefs[key]]; }
     return fallback;
 }
 
@@ -65,8 +59,7 @@
     UIGraphicsImageRenderer *renderer = [[UIGraphicsImageRenderer alloc] initWithSize:canvasSize];
     return [[renderer imageWithActions:^(UIGraphicsImageRendererContext * _Nonnull context) {
         CGSize imgSize = sysImage.size;
-        CGRect rect = CGRectMake((canvasSize.width - imgSize.width) / 2.0, (canvasSize.height - imgSize.height) / 2.0, imgSize.width, imgSize.height);
-        [sysImage drawInRect:rect];
+        [sysImage drawInRect:CGRectMake((canvasSize.width - imgSize.width)/2.0, (canvasSize.height - imgSize.height)/2.0, imgSize.width, imgSize.height)];
     }] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
 }
 
@@ -80,61 +73,47 @@
 
 - (void)fetchCurrentStateAsynchronously {
     NSTimeInterval now = [[NSDate date] timeIntervalSince1970];
-    if (now - _lastFetchTime < 0.5) return;
+    if (now - _lastFetchTime < 1.0) return;
     _lastFetchTime = now;
     
     NSString *port = [self getSetting:@"port" fallback:@"1836"];
     NSString *key = [self getSetting:@"key" fallback:@"crctdd"];
-    NSString *urlString = [NSString stringWithFormat:@"http://127.0.0.1:%@/v1/outbound", port];
-    NSURL *url = [NSURL URLWithString:urlString];
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://127.0.0.1:%@/v1/outbound", port]];
     if (!url) return;
     
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:2.0];
-    request.HTTPMethod = @"GET";
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:1.5];
     [request setValue:key forHTTPHeaderField:@"X-Key"];
     
-    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-        if (error || !data) return;
-        NSError *jsonError;
-        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
-        if (jsonError || ![json isKindOfClass:[NSDictionary class]] || !json[@"mode"]) return;
-        
-        NSString *currentMode = [[NSString stringWithFormat:@"%@", json[@"mode"]] lowercaseString];
-        BOOL newState = [currentMode isEqualToString:@"proxy"];
-        
+    [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (!data) return;
+        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        if (![json isKindOfClass:[NSDictionary class]] || !json[@"mode"]) return;
+        BOOL newState = [[[NSString stringWithFormat:@"%@", json[@"mode"]] lowercaseString] isEqualToString:@"proxy"];
         if (self->_isActuallySelected != newState) {
             self->_isActuallySelected = newState;
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [self refreshState];
-            });
+            dispatch_async(dispatch_get_main_queue(), ^{ [self refreshState]; });
         }
-    }];
-    [task resume];
+    }] resume];
 }
 
 - (void)setSelected:(BOOL)selected {
     _isActuallySelected = YES;
     [self refreshState];
     
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"SurgeModeSync" object:@"proxy"];
+
+    CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), CFSTR("com.crctdd.surgectl.selectProxy"), NULL, NULL, YES);
     
     NSString *port = [self getSetting:@"port" fallback:@"1836"];
     NSString *key = [self getSetting:@"key" fallback:@"crctdd"];
-    NSString *urlString = [NSString stringWithFormat:@"http://127.0.0.1:%@/v1/outbound", port];
-    NSURL *url = [NSURL URLWithString:urlString];
-    if (!url) return;
-    
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:3.0];
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://127.0.0.1:%@/v1/outbound", port]];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:2.0];
     request.HTTPMethod = @"POST";
     [request setValue:key forHTTPHeaderField:@"X-Key"];
     [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-    NSDictionary *body = @{@"mode": @"proxy"};
-    request.HTTPBody = [NSJSONSerialization dataWithJSONObject:body options:0 error:nil];
+    request.HTTPBody = [NSJSONSerialization dataWithJSONObject:@{@"mode": @"proxy"} options:0 error:nil];
     
-    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+    [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
         self->_lastFetchTime = 0;
-        [self fetchCurrentStateAsynchronously];
-    }];
-    [task resume];
+    }] resume];
 }
 @end
