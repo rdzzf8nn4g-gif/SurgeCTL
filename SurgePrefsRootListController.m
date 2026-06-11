2ec72e9e294dfe8c162de65d24a730e7e836c858
rdzzf8nn4g-gif <rdzzf8nn4g@privaterelay.appleid.com>
2026-06-11 20:59:18 +0800

    Update SurgePrefsRootListController.m

--- SurgePrefsRootListController.m
+++ SurgePrefsRootListController.m
@@ -13,7 +13,6 @@
     return _specifiers;
 }
 
-// 视图加载时，在右上角添加保存按钮
 - (void)viewDidLoad {
     [super viewDidLoad];
     
@@ -24,12 +23,9 @@
     self.navigationItem.rightBarButtonItem = saveButton;
 }
 
-// 用户点击确认按钮后的逻辑
 - (void)saveSettingsTapped {
-    // 1. 强制收起键盘，触发系统底层将输入的文字立即写入 Plist 文件
     [self.view endEditing:YES];
     
-    // 2. 弹窗 UI 反馈
     UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"保存成功"
                                                                    message:@"配置已保存，您可以直接使用控制中心组件或终端命令了。"
                                                             preferredStyle:UIAlertControllerStyleAlert];
@@ -39,7 +35,6 @@
                                                      handler:nil];
     [alert addAction:okAction];
     
-    // 显示弹窗
     [self presentViewController:alert animated:YES completion:nil];
 }
 
